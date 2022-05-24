3844b4ac3380fd41dedca677b093ce5de6711b68
WilliamHarveys <wharvey2005@gmail.com>
2022-05-24 11:14:10 -0400

    actual initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let randomiser = document.querySelector(".bouton");
 const legends = [
-  "Blood Houd",
+  "Blood Hound",
   "Gibraltar",
   "Life Line",
   "Pathfinder",
@@ -21,6 +21,30 @@ const legends = [
   "Ash",
   "Mad Maggie",
 ];
+const legendsColor = [
+  "#f31c37",
+  '#f8571d',
+  '#e472fc',
+  '#5cfbf4',
+  '#3f417b',
+  "#f31c37",
+  '#dcdc6b',
+  '#7c7d1b',
+  '#349208',
+  '#0e1e47',
+  '#a7a457',
+  '#7c0395',
+  '#dcdc6b',
+  '#5cfbf4',
+  '#e8c152',
+  '#8b008b',
+  '#0e1e47',
+  '#ff760d',
+  '#663a82',
+  '#26c6da',
+  '#006064',
+  '#5cfbf4'
+]
 
 randomiser.addEventListener("click", click);
 
@@ -30,10 +54,14 @@ const max = legends.length - 1;
 function click(e){
     const ran = getRandomNumberBetween(min, max);
     let LegendChoice = legends[ran]
+    let LegendColor = legendsColor[ran]
+    console.log(LegendColor)
     console.log(LegendChoice)
     console.log(ran)
-    document.getElementById("legend").textContent = LegendChoice 
-
+    
+    const legendChoiceFin = document.getElementById("legend");
+    legendChoiceFin.textContent = LegendChoice 
+  legendChoiceFin.style.color = LegendColor
 }
 
 function getRandomNumberBetween(min, max) {
